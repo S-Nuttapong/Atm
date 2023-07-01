38ea8b9c110246aba2f9cd45b2ec8afe689b9e2e
Nuttapong <Nuttapong@nuttapongs-MacBook-Pro.local>
2023-07-01 16:39:57 +0700

    chore: use query confusing output varable

--- src/shared/react-query/index.ts
+++ src/shared/react-query/index.ts
@@ -1,3 +1,12 @@
-export { QueryClient, QueryClientProvider, useMutation, useQuery } from "@tanstack/react-query";
+import { useQuery as tanstackUseQuery } from "@tanstack/react-query";
+
+export { QueryClient, QueryClientProvider, useMutation } from "@tanstack/react-query";
 export { separateQueryOperationAndResult } from "./separateQueryOperationAndResult";
+
+/**
+ * @note "isLoading" state can produce unexpected result, if not use in conjunction with "isIdle" state. For example; when doing lazy query, we can run into infinite loading, if use "isLoading" solely to represent loading state here.
+ * For majority of use cases, consider isFetching as a loading state will produce more predicable result
+ * @see https://stackoverflow.com/questions/72501651/what-state-represents-the-loading-or-data-not-fetched-yet-state-when-using-enabl
+ */
+export const useQuery = tanstackUseQuery
 
